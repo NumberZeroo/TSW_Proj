9637745790d2c4a9e2e9859eac5949529e1a4430
Emanuele A. <161335121+NumberZeroo@users.noreply.github.com>
2024-05-02 11:27:48 +0200

    File creazione DB

---
+++ Documenti/DB.sql
@@ -0,0 +1,77 @@
+DROP DATABASE IF EXISTS tswProj;
+CREATE DATABASE tswProj;
+USE tswProj;
+
+CREATE TABLE Utente (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    imgPath VARCHAR(255) NOT NULL,
+    isAdmin BIGINT NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_utente_admin CHECK (isAdmin IN (0, 1))
+);
+
+CREATE TABLE Ordine (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idUtente BIGINT UNSIGNED NOT NULL,
+    pathFattura VARCHAR(255) NOT NULL,
+    FOREIGN KEY (idUtente)
+        REFERENCES Utente (id)
+);
+
+CREATE TABLE Pet (
+    Nome VARCHAR(255) NOT NULL,
+    IdUtente BIGINT UNSIGNED NOT NULL,
+    imgPath VARCHAR(255) NOT NULL,
+    Tipo ENUM('CANE', 'GATTO', 'VOLATILE') NOT NULL,
+    Taglia ENUM('PICCOLA', 'MEDIA', 'GRANDE') NULL,
+    Sterilizzato CHAR(255) NULL,
+    DataNascita DATE NOT NULL,
+    PRIMARY KEY(Nome),
+    FOREIGN KEY(IdUtente) REFERENCES Utente(id)
+);
+
+CREATE TABLE Prodotto (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    Disponibilità TINYINT(1) NOT NULL,
+    Taglia ENUM('PICCOLA', 'MEDIA', 'GRANDE') NULL,
+    Tipo VARCHAR(255) NOT NULL,
+    MinEta INT NULL,
+    MaxEta INT NULL,
+    IVA ENUM('4', '10', '22') NOT NULL,
+    Prezzo BIGINT NOT NULL,
+    Sterilizzati TINYINT(1) NULL,
+    imgPath VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Carrello (
+    IdUtente BIGINT UNSIGNED NOT NULL,
+    idProdotto BIGINT UNSIGNED NOT NULL,
+    Quantita BIGINT NOT NULL,
+    PRIMARY KEY(IdUtente, idProdotto),
+    FOREIGN KEY(IdUtente) REFERENCES Utente(id),
+    FOREIGN KEY(idProdotto) REFERENCES Prodotto(id)
+);
+
+CREATE TABLE OrderItem (
+    idItem BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    IdOrdine BIGINT UNSIGNED NOT NULL,
+    Prezzo BIGINT NOT NULL,
+    Quantita BIGINT NOT NULL,
+    FOREIGN KEY(IdOrdine) REFERENCES Ordine(id)
+);
+
+CREATE TABLE Recensione (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    idUtente BIGINT UNSIGNED NOT NULL,
+    Titolo VARCHAR(255) NOT NULL,
+    Commento VARCHAR(255) NOT NULL,
+    Valutazione BIGINT NOT NULL,
+    Data DATE NOT NULL,
+    idProdotto BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY(id, idUtente),
+    FOREIGN KEY(idUtente) REFERENCES Utente(id),
+    FOREIGN KEY(idProdotto) REFERENCES Prodotto(id)
+);
