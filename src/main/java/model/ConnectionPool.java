1946d3c5f66d1dfd7a18aa037be9d4d523ef5ca1
Emanuele A. <161335121+NumberZeroo@users.noreply.github.com>
2024-05-03 17:09:21 +0200

    Stampa errore

--- src/main/java/model/ConnectionPool.java
+++ src/main/java/model/ConnectionPool.java
@@ -6,43 +6,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionPool {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/tswProj" +
-            "?useUnicode=true&useJDBCCompliantTimezoneShift=true" +
-            "&useLegacyDatetimecode=false&serverTimezone=UTC";
 
-    private static final String username = "root";
-    private static final String pswd = "e.amoreroot";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/tswproj";
 
-    private Connection con;
+    private static final String USERNAME = "root";
+
+    private static final String PASSWORD = "e.amoreroot";
+
+    private static final int INITIAL_POOL_SIZE = 5;
+
 
     public ConnectionPool(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(JDBC_URL, username, pswd);
         }
         catch(Exception e){
             System.out.println("Connessione fallita");
         }
     }
 
-    /*private static final String USERNAME = "root"; //todo
-    private static final String PASSWORD = "e.amoreroot"; //todo
-    private static final int INITIAL_POOL_SIZE = 5;
-
     private static List<Connection> connectionPool = new ArrayList<>();
-
-    static {
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }catch(ClassNotFoundException e){
-            e.printStackTrace();
-        }
-    }*/
 
     public static synchronized Connection getConnection() {
             try {
-                return DriverManager.getConnection(JDBC_URL, username, pswd);
+                Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+                return conn;
             } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
                 return null;
             }
     }
